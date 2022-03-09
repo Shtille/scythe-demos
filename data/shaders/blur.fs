4bef59ff8bf8c8a03163d7a25b91bfd17443e350
Vladimir <v.shtille@gmail.com>
2022-03-10 02:51:25 +0300

    fixed blur shader

--- data/shaders/blur.fs
+++ data/shaders/blur.fs
@@ -16,6 +16,6 @@ void main()
 	color += texture(u_texture, v_texcoord + vec2( 0.0          ,  0.0          ))*0.3125;
 	color += texture(u_texture, v_texcoord + vec2( 1.0*u_scale.x,  1.0*u_scale.y))*0.234375;
 	color += texture(u_texture, v_texcoord + vec2( 2.0*u_scale.x,  2.0*u_scale.y))*0.09375;
-	color += texture(u_texture, v_texcoord + vec2( 3.0*u_scale.x, -3.0*u_scale.y))*0.015625;
+	color += texture(u_texture, v_texcoord + vec2( 3.0*u_scale.x,  3.0*u_scale.y))*0.015625;
 	out_color = vec4(color.xyz, 1.0);
 }
